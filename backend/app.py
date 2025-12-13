c425d9d4decf7feebf33a5a7136788918b10b871
Tymofii Nasobko <timofiyj@gmail.com>
2025-12-13 15:41:03 +0200

    feat: add app

---
+++ backend/app.py
@@ -0,0 +1,18 @@
+import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
+from router import router
+
+app = FastAPI()
+app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
